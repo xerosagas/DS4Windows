8ee6cea22ca7e974c644cf51871717d3288fed88
schmaldeo <michal.skrzyszowski@hotmail.com>
2024-08-08 19:28:44 +0200

    Remove state list from Debouncer

--- DS4Windows/DS4Library/Debouncer.cs
+++ DS4Windows/DS4Library/Debouncer.cs
@@ -30,10 +30,6 @@ public class Debouncer(TimeSpan duration)
 
     private class DebouncerInstance(TimeSpan duration)
     {
-        /// <summary>
-        ///     List of last states of the button.
-        /// </summary>
-        private readonly List<bool> _state = new();
         private bool _previousState;
         private bool _currentlyDebouncing;
         private long _debounceStartTime;
@@ -73,21 +69,15 @@ public class Debouncer(TimeSpan duration)
         private void StopDebouncing()
         {
             _currentlyDebouncing = false;
-            _state.Clear();
         }
 
         private bool Debounce(bool reading, long timestamp)
         {
-            _state.Add(reading);
-
             // if the duration hasn't been reached yet, we return true as if the button was pressed all this time
             if (timestamp - _debounceStartTime < Duration.TotalMilliseconds * TimeSpan.TicksPerMillisecond) return true;
 
-            // this detects whether the button was actually released, if all values are false in the state window, this
-            // function returns false, if any of them are true, a bounce (or hold) is detected, so we return true
-            var trueState = _state.Any(x => x);
             StopDebouncing();
-            return trueState;
+            return reading;
         }
     }
 }
