87314778f3be1496452f8e842936d89e241a3b0e
schmaldeo <michal.skrzyszowski@hotmail.com>
2024-08-30 17:36:47 +0200

    Define beta version

--- DS4Windows/DS4Forms/MainWindow.xaml.cs
+++ DS4Windows/DS4Forms/MainWindow.xaml.cs
@@ -199,7 +199,7 @@ namespace DS4WinWPF.DS4Forms
 
             // Log exceptions that might occur
             Util.LogAssistBackgroundTask(tempTask);
-
+#if !BETA_VERSION
             tempTask = Task.Delay(100).ContinueWith((t) =>
             {
                 int checkwhen = Global.CheckWhen;
@@ -218,6 +218,7 @@ namespace DS4WinWPF.DS4Forms
                 //    return;
                 //}
             });
+#endif
             Util.LogAssistBackgroundTask(tempTask);
         }
 
