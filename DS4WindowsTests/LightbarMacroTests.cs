b3b66b96d2f285b92eff0be031bf9533d9befdbd
schmatteo <michal.skrzyszowski@hotmail.com>
2024-10-11 12:48:57 +0200

    Update tests

--- DS4WindowsTests/LightbarMacroTests.cs
+++ DS4WindowsTests/LightbarMacroTests.cs
@@ -9,38 +9,37 @@ public class LightbarMacroTests
     [TestMethod]
     public void CheckCompile()
     {
-        var binding = new OutBinding();
-        binding.UseLightbarMacro = true;
-        binding.LightbarMacroTrigger = LightbarMacroTrigger.Press;
-        binding.LightbarMacro =
-        [
-            new LightbarMacroElement(new DS4Color(255, 255, 255), 100),
-            new LightbarMacroElement(new DS4Color(255, 0, 0), 50),
-            new LightbarMacroElement(new DS4Color(0, 128, 0), 1000),
-            new LightbarMacroElement(new DS4Color(0, 0, 0), 5)
-        ];
-
+        var macro = new LightbarMacro(
+            true,
+            [
+                new LightbarMacroElement(new DS4Color(255, 255, 255), 100),
+                new LightbarMacroElement(new DS4Color(255, 0, 0), 50),
+                new LightbarMacroElement(new DS4Color(0, 128, 0), 1000),
+                new LightbarMacroElement(new DS4Color(0, 0, 0), 5)
+            ],
+            LightbarMacroTrigger.Press);
         var expectedStr = "True/255,255,255:100;255,0,0:50;0,128,0:1000;0,0,0:5/Press";
-        var output = binding.CompileLightbarMacro();
+        var output = macro.Compile();
         Assert.AreEqual(output, expectedStr);
     }
 
     [TestMethod]
     public void CheckParse()
     {
-        var binding = new OutBinding();
         var incomingString = "True/255,255,255:100;255,0,0:50;0,128,0:1000;0,0,0:5/Press";
-        binding.ParseLightbarMacro(incomingString);
+        var macro = new LightbarMacro();
+        macro.Parse(incomingString);
 
-        Assert.AreEqual(binding.UseLightbarMacro, true);
-        Assert.AreEqual(binding.LightbarMacroTrigger, LightbarMacroTrigger.Press);
-        var macro = new LightbarMacroElement[]
+        Assert.AreEqual(macro.Active, true);
+        Assert.AreEqual(macro.Trigger, LightbarMacroTrigger.Press);
+        var expected = new LightbarMacroElement[]
         {
             new(new DS4Color(255, 255, 255), 100),
             new(new DS4Color(255, 0, 0), 50),
             new(new DS4Color(0, 128, 0), 1000),
             new(new DS4Color(0, 0, 0), 5)
         };
-        CollectionAssert.AreEqual(binding.LightbarMacro, macro);
+        CollectionAssert.AreEqual(macro.Macro, expected);
+        CollectionAssert.AreEqual(macro.ObservableMacro, expected);
     }
 }
